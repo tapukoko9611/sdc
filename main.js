f357626705aabee4fdc2359776dd5afdb05fa4ae
tapukoko9611 <tapukoko9611@gmail.com>
2025-10-14 13:14:47 +0000

    visualizing neural network

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const carCanvas=document.getElementById("carCanvas");
 carCanvas.width=200;
-// const networkCanvas=document.getElementById("networkCanvas");
-// networkCanvas.width=300;
+const networkCanvas=document.getElementById("networkCanvas");
+networkCanvas.width=300;
 
 const carCtx = carCanvas.getContext("2d");
-// const networkCtx = networkCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
 
 const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
 const car=new Car(road.getLaneCenter(1),100,30,50,"AI");
@@ -14,14 +14,14 @@ const traffic=[
 
 animate();
 
-function animate(){
+function animate(time){
     for(let i=0;i<traffic.length;i++){
         traffic[i].update(road.borders,[]);
     }
     car.update(road.borders,traffic);
 
     carCanvas.height=window.innerHeight;
-    // networkCanvas.height=window.innerHeight;
+    networkCanvas.height=window.innerHeight;
 
     carCtx.save();
     carCtx.translate(0,-car.y+carCanvas.height*0.7);
@@ -34,5 +34,7 @@ function animate(){
 
     carCtx.restore();
 
+    networkCtx.lineDashOffset=-time/50;
+    Visualizer.drawNetwork(networkCtx,car.brain);
     requestAnimationFrame(animate);
 }
